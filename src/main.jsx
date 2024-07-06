c25fb17821104f646c93e95a030dffaa6a9ec5de
Nakshatra Manglik <nakshatramanglik14@gmail.com>
2024-07-06 22:59:18 +0530

    TopG Website analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import React from "react";
-
+import { Analytics } from "@vercel/analytics/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>
 );
